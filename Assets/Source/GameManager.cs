b77bfe7d8ced9d5e2f43f28e005754243d8bc1ef
Jean Catanho <jensmcatanho@gmail.com>
2018-01-15 23:00:47 -0200

    gameplay: transform maze in a generic container

--- Assets/Source/GameManager.cs
+++ Assets/Source/GameManager.cs
@@ -19,7 +19,7 @@ public class GameManager : MonoBehaviour {
 
 	void Start () {
         mazeFactory = GameObject.Find("GameManager").GetComponent<Rendering.MazeFactory>();
-        Gameplay.Maze gpMaze = dfsFactory.CreateMaze(mazeLength, mazeWidth, cellSize);
+        Gameplay.Maze<Gameplay.Cell> gpMaze = dfsFactory.CreateMaze(mazeLength, mazeWidth, cellSize);
         Rendering.Maze maze = mazeFactory.CreateMaze(gpMaze);
 
 		Instantiate (player, new Vector3 (1.0f * cellSize, 1.0f, 1.0f * cellSize), new Quaternion());
