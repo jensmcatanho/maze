3b81790ca602302ecac272d9a5181a18fccf0a5a
Jean Catanho <jensmcatanho@gmail.com>
2018-01-20 05:11:43 -0200

    refactor: create gameplay system and rename main system

--- Assets/Source/Gameplay/GameplaySystem.cs
+++ Assets/Source/Gameplay/GameplaySystem.cs
@@ -1,7 +1,43 @@
+using UnityEngine;
+
 namespace Gameplay {
 
-public class GameplaySystem {
-    
+public class GameplaySystem : MonoBehaviour {
+    static GameplaySystem s_Instance = null;
+
+    DFSFactory mazeFactory;
+
+    public void CreateListeners() {
+		EventManager.Instance.AddListener<global::Events.CreateNewMaze>(CreateMaze);
+    }
+
+    void Awake() {
+        CreateListeners();
+        s_Instance = null;
+        
+    }
+
+    public static GameplaySystem Instance {
+        get {
+            if (s_Instance == null) {
+                s_Instance = GameObject.FindObjectOfType (typeof(GameplaySystem)) as GameplaySystem;
+            }
+            return s_Instance;
+        }
+    }
+
+    void CreateMaze(global::Events.CreateNewMaze e) {
+        switch (e.mazeType) {
+            case MazeType.DFS:
+                mazeFactory = new DFSFactory();
+                break;
+            case MazeType.Prim:
+                //mazeFactory = new PrimFactory();
+                break;
+        }
+
+        mazeFactory.CreateMaze(e.mazeLength, e.mazeWidth, e.cellSize);
+    }
 }
 
 }
