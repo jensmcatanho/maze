b60e6a57794b2b31e4bde696a32b7982b08c7ed9
Jean Catanho <jensmcatanho@gmail.com>
2018-01-20 21:38:57 -0200

    gameplay: organize code

--- Assets/Source/Gameplay/Events/MazeReadyEvent.cs
+++ Assets/Source/Gameplay/Events/MazeReadyEvent.cs
@@ -4,14 +4,9 @@ namespace Gameplay {
 namespace Events {
 
 public class MazeReady<T> : Core.Events.GameEvent {
-    public Maze<T> maze {
-        get;
-        private set;
-    }
+    public Maze<T> m_Maze { get; private set; }
 
-    public MazeReady(Maze<T> m) {
-        maze = m;
-    }
+    public MazeReady(Maze<T> m) { m_Maze = m; }
 }
 
 }
